d9877bc6519c99dd31564d374189d214392d247f
David Ermakov <62952539+Goldmagination@users.noreply.github.com>
2025-05-28 17:42:35 +0200

    bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:spend_app/src/core/services/goal_service.dart';
 import 'src/app.dart'; // Adjusted import path
 import 'src/core/models/article_model.dart'; // Import Article model
-import 'src/core/models/goal_model.dart';   // Import Goal model
-// Import generated adapters
-import 'src/core/models/article_model.g.dart';
-import 'src/core/models/goal_model.g.dart';
+import 'src/core/models/goal_model.dart'; // Import Goal model
 
-void main() async { // Make main async
-  WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter bindings are initialized
+void main() async {
+  // Make main async
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure Flutter bindings are initialized
   await Hive.initFlutter(); // Initialize Hive
 
   // Register adapters
